4c21e7aac51be3701880d3f5affb0991385c9424
patchw <patchwhyte@gmail.com>
2018-10-10 18:55:27 +0100

    implemented stack

--- Stack.h
+++ Stack.h
@@ -2,11 +2,63 @@
 #define STACK_H
 
 #include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+
 using std::vector;
+using std::string;
 
 // TODO: Your code goes here
 
+class Stack {
 
+private:
+    vector<int> vStack;
+    vector<char> tokens;
+    int count = 0;
+
+public:
+
+    Stack()
+            : vStack(0)
+    {
+
+    }
+
+    bool empty(){
+        if (count == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    void push(double item){
+        vStack.push_back(item);
+        count++;
+
+    }
+
+    double pop(){
+        double temp = vStack.back();
+        vStack.pop_back();
+        count--;
+        return temp;
+    }
+
+
+};
+
+
+
+double evaluate(string input){
+
+}
 
 // Do not write anything below this line
 
